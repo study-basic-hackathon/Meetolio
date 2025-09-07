c6c3bb6fff0951683a5628cc28946bf1824c1a76
chibitaka0320 <taka.araat32@gmail.com>
2025-09-07 10:43:07 +0900

    パスワードエンコーダーの作成

--- backend/src/main/java/com/meetolio/backend/common/security/SecurityConfig.java
+++ backend/src/main/java/com/meetolio/backend/common/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
@@ -23,5 +25,11 @@ public class SecurityConfig {
                 .anyRequest().permitAll());
 
         return http.build();
+    }
+
+    /** パスワードハッシュ化用 */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
