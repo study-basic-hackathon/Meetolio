24cd795b3fc19dae3baf089640d606c0cbcf5154
chibitaka0320 <taka.araat32@gmail.com>
2025-09-09 08:32:47 +0900

    JWTの検証、解析処理追加

--- backend/src/main/java/com/meetolio/backend/common/security/JwtService.java
+++ backend/src/main/java/com/meetolio/backend/common/security/JwtService.java
@@ -3,7 +3,9 @@ package com.meetolio.backend.common.security;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 /** Jwtトークン操作Service */
 @Service
@@ -15,11 +17,24 @@ public class JwtService {
     /** アルゴリズム */
     private final Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
 
+    /** JWTトークン検証用 */
+    private final JWTVerifier verifier = JWT.require(algorithm).build();
+
     /** JWT作成 */
     public String generateToken(Integer userId) {
 
         // subにuserId入れる
         // TODO: 有効期限の設定がなし
         return JWT.create().withSubject(userId.toString()).sign(algorithm);
+    }
+
+    /** JWT解析 */
+    public DecodedJWT verifyToken(String token) {
+        return verifier.verify(token);
+    }
+
+    /** Subject(UserId)の抽出 */
+    public String extractUserId(String token) {
+        return verifyToken(token).getSubject();
     }
 }
